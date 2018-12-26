c811a184fe0b9086bca5214e6edddf91db67edb8
gywndi <gywndi@gmail.com>
2018-12-26 16:50:17 +0900

    first modify

--- SQL-mytest.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO test(`a`,`b`,`c`,`d`,`e`) VALUES ('abc\ndefadf\nadfdsaf','d\'ef','"ssss"','$adfadf','사람');
-INSERT INTO test(`a`,`b`,`c`,`d`,`e`) VALUES ('__','`dddsf`','ㅋㅋㅋ','%%%하하','사람');
-COMMIT;
